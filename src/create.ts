1d6d7f882de01ac73c49d070ea4037ed6bf98f56
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-02-26 13:42:44 +0000

    Initial commit

---
+++ src/create.ts
@@ -0,0 +1,40 @@
+import fs from "fs";
+import path from 'path';
+import extract from 'extract-zip';
+import fse from 'fs-extra';
+import errorLog from "./errorLog";
+
+export async function downloadAndExtractRepo(folder: string, repo: string, author: string = "Kars1996") {
+    const url = `https://github.com/${author}/${repo}/archive/refs/heads/main.zip`;
+    const response = await fetch(url);
+    if (!response.ok){
+        errorLog(`Failed to download repo: ${response.statusText}`)
+        throw new Error()
+    }
+
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const zipPath = path.join(folder, 'repo.zip');
+    const outputPath = path.resolve(folder); // Convert the folder path to an absolute path
+
+    if (!fs.existsSync(outputPath)) {
+        await fs.mkdirSync(outputPath, { recursive: true });
+    }
+
+    await fs.writeFileSync(zipPath, buffer);
+
+    await extract(zipPath, { dir: outputPath });
+
+    await fs.unlinkSync(zipPath);
+
+    const sourceFolder = path.join(outputPath, `${repo}-main`);
+    const files = await fse.readdir(sourceFolder);
+    for (const file of files) {
+        const sourceFilePath = path.join(sourceFolder, file);
+        const destinationFilePath = path.join(outputPath, file);
+        await fse.move(sourceFilePath, destinationFilePath, { overwrite: true });
+    }
+
+    await fse.remove(sourceFolder);
+    await fse.remove(path.join(outputPath, '.git'));
+}
