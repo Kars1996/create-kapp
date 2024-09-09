45b684b423f4e61792469c1647aba64a31a611a5
Kars1996 <k@kars.bio>
2024-09-09 10:57:13 +0100

    feat: Update Build Code

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,27 +1,54 @@
 const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const bump = (version) => {
+    const parts = version.split(".");
+    parts[2] = parseInt(parts[2], 10) + 1;
+    return parts.join(".");
+};
+
+const update = (filePath, newVersion) => {
+    const packageJson = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    packageJson.version = newVersion;
+    fs.writeFileSync(filePath, JSON.stringify(packageJson, null, 2), "utf8");
+    return newVersion;
+};
 
 const main = () => {
-    console.log("\x1b[36mStarting build...\x1b[37m");
+    const packagePath = path.join(__dirname, "../package.json");
+    const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
+    const current = packageJson.version;
+    const newVersion = bump(current);
+
+    console.log(
+        `\x1b[36mCurrent version: \x1b[38;2;77;143;255m${current}\x1b[36m. Bumping to \x1b[38;2;77;143;255m${newVersion}\x1b[36m...\x1b[37m`
+    );
+
+    update(packagePath, newVersion);
+
     exec("tsc", (err, stdout, stderr) => {
         if (err) {
             console.log(
-                "\x1b[31m❌ Build failed with the following error: \x1b[37m"
+                "\x1b[31m❌ Build failed with the following error:\x1b[37m"
             );
             console.log(stdout);
             return;
         }
-        console.log("\x1b[32mBuild successful! \x1b[36m");
-        exec("npm publish", (err, stdout, stderr) => {
+        console.log("\x1b[32mBuild successful!\x1b[36m");
+
+        exec("npm publish --yes", (err, stdout, stderr) => {
             if (err) {
                 console.log(
-                    "\x1b[31mFailed to publish repo with the following error\x1b[37m"
+                    "\x1b[31mFailed to publish repo with the following error:\x1b[37m"
                 );
                 console.log(stdout);
                 return;
             }
             console.log(
-                "\x1b[36mSuccessfully published and bumped version\x1b[37m"
+                `\x1b[36mSuccessfully published and bumped version to ${newVersion}\x1b[37m`
             );
+
             exec("npm i create-kapp@latest -g", (err, stdout, stderr) => {
                 if (err) {
                     console.log(
@@ -51,7 +78,7 @@ try {
             }
             console.log(stdout);
             console.log(
-                "\x1b[32mNode modules successfully installed. \x1b[33mRestarting in 3 seconds... \x1b[37m"
+                "\x1b[32mNode modules successfully installed. \x1b[33mRestarting in 3 seconds...\x1b[37m"
             );
             setTimeout(() => {
                 exec("node scripts/publish.js", (err, stdout, stderr) => {
