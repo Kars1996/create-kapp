493e4255496fe5cc4edd6c24d7ff79fc2ec182fa
Kars <k@kars.bio>
2024-10-23 10:48:22 +0100

    chore: Other changes

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,13 +1,17 @@
-const templateOptions: Record<string, string[]> = {
+import { version as packageVersionImport } from "../../package.json";
+
+export const templateOptions: Record<string, string[]> = {
     "Next.js": ["Template", "APITemplate"],
     "Discord.js": ["DJS14Template", "DJS14Base"],
     General: ["NextTemplate", "APITemplate"],
 };
 
-const icons: Record<string, string> = {
+export const icons: Record<string, string> = {
     "Next.js": "▲",
     "Discord.js": "§",
     General: "∂",
 };
 
-type TemplateCategory = keyof typeof templateOptions;
+export type TemplateCategory = keyof typeof templateOptions;
+
+export const packageVersion = packageVersionImport;
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,24 @@
 // ? Types Here
+export type AnalyticsEvent =
+    | {
+          eventName: "cli_started";
+      }
+    | {
+          eventName: "cli_completed";
+          template: string;
+      }
+    | {
+          eventName: "cli_selection_complete";
+          template: string;
+      }
+    | {
+          eventName: "cli_failed";
+          template: string;
+      };
+
+export type AnalyticsEventWithCommonProperties = AnalyticsEvent & {
+    id: string;
+    timestamp: Date;
+    os: string;
+    cliversion: string;
+};
