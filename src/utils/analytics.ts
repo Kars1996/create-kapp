c1f9179ac33f3c169ed701a4eb94ed193d69c8b8
Kars <k@kars.bio>
2024-10-23 10:48:13 +0100

    feat: Analytics Code
    Trying to make it as anonymous as possible

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,11 +1,73 @@
-// TODO: Send Analytics for certain events
+import os from "os";
+import { packageVersion } from "../data/consts";
+import { AnalyticsEvent } from "../types";
+import crypto from "crypto";
 
 export default class AnalyticsManager {
-    private _analyticsUrl: string = "https://kars.bio/api/cli/analytics";
-    public static async sendAnalytics(): Promise<void> {}
+    private static readonly _analyticsUrl: string =
+        "https://kars.bio/api/cli/analytics";
 
-    private getAnalyticsId(): string {
-        // TODO: Implement analytics ID retrieval
-        return "example-analytics-id";
+    public static async sendAnalytics(
+        analyticsEvent: AnalyticsEvent
+    ): Promise<void> {
+        const analyticsId = this.getAnalyticsId();
+        const payload = {
+            ...analyticsEvent,
+            id: analyticsId,
+            timestamp: new Date().toISOString(),
+            package: "create-kapp",
+            cliversion: packageVersion,
+            os: os.platform(),
+        };
+        try {
+            await fetch(this._analyticsUrl, {
+                method: "POST",
+                body: JSON.stringify(payload),
+                headers: {
+                    "Content-Type": "application/json",
+                    "api-version": "0",
+                },
+            });
+        } catch (e) {
+            ("Tomfoolery and such are not supported");
+        }
+    }
+
+    private static getAnalyticsId(): string {
+        const mac = this.getMacAddress();
+        const salt = this.getSystemSalt();
+        const hash = crypto
+            .createHash("sha256")
+            .update(mac + salt)
+            .digest("hex");
+
+        return [
+            hash.substring(0, 8),
+            hash.substring(8, 12),
+            hash.substring(12, 16),
+            hash.substring(16, 20),
+            hash.substring(20, 32),
+        ].join("-");
+    }
+
+    private static getMacAddress(): string {
+        const interfaces = os.networkInterfaces();
+        for (const name of Object.keys(interfaces)) {
+            for (const iface of interfaces[name]!) {
+                if (iface.mac && iface.mac !== "00:00:00:00:00:00") {
+                    return iface.mac;
+                }
+            }
+        }
+        return "00:00:00:00:00:00";
+    }
+
+    private static getSystemSalt(): string {
+        const cpu = os.cpus()[0].model;
+        const hostname = os.hostname();
+        return crypto
+            .createHash("sha256")
+            .update(cpu + hostname)
+            .digest("hex");
     }
 }
