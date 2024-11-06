b3853d8438efadb6bd816bb67b3fccad52f05f79
Kars <k@kars.bio>
2024-11-06 14:39:05 +0000

    chore: Wifi Check

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,14 +1,22 @@
-// TODO: Validation to remove bugs
-
-import UI from "./UI"
+import UI from "./UI";
 
 export default class ValidationManager {
-    public static definedCheck(value: unknown): asserts value is NonNullable<typeof value> {
-        if (value === undefined || value === null ) {
-            UI.print(`${value as unknown as string} cannot be undefined`)
-            UI.footer()
-            process.exit(1)
+    public static definedCheck(
+        value: unknown
+    ): asserts value is NonNullable<typeof value> {
+        if (value === undefined || value === null) {
+            UI.print(`${value as unknown as string} cannot be undefined`);
+            UI.footer();
+            process.exit(1);
         }
     }
-    
+
+    public static async wifiCheck(): Promise<boolean> { // TODO: More Robust Check
+        try {
+            await fetch("google.com");
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
 }
